745a87951b04652979c21661a80916abe75043ec
Andrei Cioran <Andrei Cioran>
2016-03-27 23:39:00 +0300

    Version 3.0, with major improvemnts: -autostarts everything -clean exit -code refactored (added worker for keyboard handling)

--- andrQuickRemote.py
+++ andrQuickRemote.py
@@ -15,30 +15,38 @@ VERSION 2.0 uses minicap on phone: much faster
 VERSION 3.0 uses Remote keyboard and is much faster at writing
 
 TODO:
-    -clean-up and refactoring
-    -autostart of services on phone and adb forward commands
+    -[DONE]clean-up and refactoring
+    -[DONE, needs more testing]autostart of services on phone and adb forward commands
     -sleep when out of focus (needs img display reimplementation)
     -faster swipes/touches
 
 BUGS:
-    -does not configure phone on start (start minicap, adb forward)
-    -start might freeze
+    -[FIXED, in testing]does not configure phone on start (start minicap, adb forward)
+    -[FIXED? not sure]start might freeze
+    -long start time due to sleep in Minicap part....should find better solution!
     -keyboard does not work on lockscreen (but mouse does)
-    -ocasionally it freezes (not sure why, the image receive does not work)
-    -no clean exit
+    -[FIXED? not sure]ocasionally it freezes (not sure why, the image receive does not work)
+    -[FIXED] no clean exit
 
 USE:
     -install minicap on phone (see repository for howto)
     -install "Remote keyboard" app on phone from Play Store
-    -run "adb shell LD_LIBRARY_PATH=/data/local/tmp/minicap-devel /data/local/tmp/minicap-devel/minicap -P 320x480@320x480/0"
-    -run "adb forward tcp:1313 localabstract:minicap" for minicap
-    -run "adb forward tcp:2323 tcp:2323" for keyboard
     -left-click on screen for touch event
     -right-click, drag and let go for swipe
     -type for inputting text to the current field
     -CTRL+V for pasting text :)
+
+COMMANDS THAT ARE RUN BY THE APP ON STARTUP:
+    -run "adb shell LD_LIBRARY_PATH=/data/local/tmp/minicap-devel /data/local/tmp/minicap-devel/minicap -P 320x480@320x480/0"
+    -run "adb forward tcp:1313 localabstract:minicap" for minicap
+    -run "adb forward tcp:2323 tcp:2323" for keyboard
+
+COMMANDS THAT ARE RUN BY THE APP ON STOP:
+    -run "adb forward --remove tcp:1313"
+    -run "adb forward --remove tcp:2323"
+
 '''
-import subprocess
+import select
 import io
 from PIL import Image, ImageTk
 import tkinter as tk
@@ -48,77 +56,52 @@ import time
 import random
 import socket
 import struct
+import pexpect
 import telnetlib
+import subprocess
 
 class LiveAndroidFeed():
-    def __init__(self, parent):
+    def __init__(self, parent, xsize=320, ysize=480):
         self.parent = parent
-        self.buf = io.BytesIO()
-        self.wordToSend = b""
-        self.swipeInfo = [-1, -1, -1, -1]
-        self.imgCounter = 0
-        self.refreshTime = 1 #ms
-        self._job = None
-        self.LOCAL_IP = '127.0.0.1'
-        self.IMG_PORT = 1313
-        self.KBD_PORT = 2323
 
-        self.sock = self.initMinicap(self.LOCAL_IP, self.IMG_PORT)
-        self.keyboard = telnetlib.Telnet(self.LOCAL_IP, self.KBD_PORT)
-        self.parseGlobalHeader()
+        self.time_refresh = 1 #ms
 
-        testImg = self.imgGetFromDevice()
-        xsize, ysize = testImg.size
+        self.tmp_swipeInfo = [-1, -1, -1, -1]
 
-        self.canvas = tk.Canvas(self.parent, width=xsize, height=ysize)
-        self.canvas.grid(row=0, column=0)
+        self.counter_imgs = 0
+        self.counter_emptyimgs = 0
 
-        tkimg = ImageTk.PhotoImage(testImg)
-        self.tmp_image = tkimg #KEEP REFERENCE TO IMAGE!! OTHERWISE IT DOESN"T SHOW!
-        self.image_on_canvas = self.canvas.create_image(xsize/2, ysize/2, image=tkimg)
+        self.job_imgUpdateCanvas = None
+
+        self.queue_adb = queue.Queue()
+        self.queue_img = queue.Queue()
+        self.queue_kbd = queue.Queue()
+
+        self.MAX_EMPTYIMGS = 180
+        self.IP_LOCAL = '127.0.0.1'
+        self.PORT_KBD = 2323
+        self.PORT_IMG = 1313
+
+        self.w_adb = ADBWorker(self.queue_adb)
+        self.w_img = MinicapWorker(self.queue_img, self.IP_LOCAL, self.PORT_IMG)
+        self.w_kbd = KBDWorker(self.queue_kbd, self.IP_LOCAL, self.PORT_KBD)
+
+        self.tk_canvas = tk.Canvas(self.parent, width=xsize, height=ysize)
+        self.tk_canvas.grid(row=0, column=0)
+        self.tk_img_on_canvas = self.tk_canvas.create_image(xsize/2, ysize/2, image=None)
 
         #Init everything
         self.initMouse()
         self.initKeys()
-        #self.initSleepControls()
+        self.parent.protocol("WM_DELETE_WINDOW", self.closeAll)
 
-        #Worker for ADB sends
-        self.queue = queue.Queue()
-        ADBWorker(self.queue).start()
+        #Workers for ADB sends and minicap
+        self.w_adb.start()
+        self.w_img.start()
+        self.w_kbd.start()
 
         #Displays the image and schedules the next refresh
-        self.PER_kbdSendText()
         self.imgUpdateCanvas()
-
-    def initMinicap(self, TCP_IP, TCP_PORT):
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        sock.connect((TCP_IP, TCP_PORT))
-
-        return sock
-
-    def parseGlobalHeader(self):
-        data = self.sock.recv(24)
-        vers = struct.unpack_from("B", data, 0)[0]
-        print("VERSION =", vers)
-
-        header_size = struct.unpack_from("B", data, 1)[0]
-        print("H SIZE =", header_size)
-
-        proc_pid = struct.unpack_from("I", data, 2)[0]
-        print("PID =", proc_pid)
-
-        x_real = struct.unpack_from("I", data, 6)[0]
-        y_real = struct.unpack_from("I", data, 10)[0]
-        print("Real display size: ", x_real, "x", y_real)
-
-        x_virt = struct.unpack_from("I", data, 14)[0]
-        y_virt = struct.unpack_from("I", data, 18)[0]
-        print("Real display size: ", x_virt, "x", y_virt)
-
-        disp_or = struct.unpack_from("B", data, 22)[0]
-        print("ORIENTATION =", disp_or)
-        qirks = struct.unpack_from("B", data, 23)[0]
-        print("qirks =", qirks)
 
     def initMouse(self):
         #Use b1 for touch events
@@ -133,56 +116,48 @@ class LiveAndroidFeed():
         self.parent.bind("<Return>", self.cbReturnPress)
         self.parent.bind("<Control-v>", self.cbPaste)
 
-    def initSleepControls(self):
-        self.parent.bind("<FocusIn>", self.cbInFocus)
-        self.parent.bind("<FocusOut>", self.cbOutFocus)
+    def closeAll(self):
+        self.queue_adb.put(None)
+        self.queue_kbd.put(None)
+        self.w_img.closeMinicap()
+
+        self.parent.destroy()
 
 
     '''
     -----------------------SCREEN PROCESSING
     '''
-    def imgGetFromDevice(self):
-        data = self.sock.recv(4)
-        frame_size = struct.unpack_from("I", data, 0)[0]
-
-        self.buf.seek(0)
-        len_read = 0
-        while len_read < frame_size:
-            print("SOCK RECV")
-            data = self.sock.recv(8192)
-            print("SOCK RECVed")
-            len_read += len(data)
-
-            self.buf.write(data)
-        self.buf.seek(0)
-
-        img = Image.open(self.buf)
-        return img
-
     def imgUpdateCanvas(self):
         #Just a small stat
-        self.dbg("called", self.imgCounter)
-        self.imgCounter += 1
+        self.dbg("called", self.counter_imgs, self.counter_emptyimgs, "\r")
+        self.counter_imgs += 1
 
         #Only do this if the app has focus
-        testImg = self.imgGetFromDevice()
-        tkimg = ImageTk.PhotoImage(testImg)
-        self.tmp_image = tkimg #KEEP REFERENCE TO IMAGE!! OTHERWISE IT DOESN"T SHOW!
+        bad = False
+        try:
+            testImg = self.queue_img.get(block=False)
+        except queue.Empty:
+            bad = True
 
-        self.canvas.itemconfig(self.image_on_canvas, image=tkimg)
-        self._job = self.parent.after(self.refreshTime, self.imgUpdateCanvas)
-    '''
-    -----------------------FOCUS CALLBACKS
-    def cbInFocus(self, event):
-        self.dbg("in focus")
-        self.refreshTime = 1    #ms
-        self.parent.after_cancel(self._job)
-        self.imgUpdateCanvas()
+        try:
+            tkimg = ImageTk.PhotoImage(testImg)
+        except:
+            bad = True
 
-    def cbOutFocus(self, event):
-        self.dbg("LOST FOCUS")
-        self.refreshTime = 60000 #ms = 30s
-    '''
+        if bad is True:
+            self.dbg("EMPTY IMAGE")
+            self.counter_emptyimgs += 1
+            if self.counter_emptyimgs > self.MAX_EMPTYIMGS:
+                print("TODO")
+                exit(-1)
+        else:
+            self.counter_emptyimgs = 0
+
+            self.tmp_image = tkimg #KEEP REFERENCE TO IMAGE!! OTHERWISE IT DOESN"T SHOW!
+
+            self.tk_canvas.itemconfig(self.tk_img_on_canvas, image=tkimg)
+            self.queue_img.task_done()
+        self.job_imgUpdateCanvas = self.parent.after(self.time_refresh, self.imgUpdateCanvas)
 
     '''
     -----------------------MOUSE CALLBACKS
@@ -192,21 +167,21 @@ class LiveAndroidFeed():
         self.adbSendTouch(event.x, event.y)
 
     def cbSwipeStart(self, event):
-        if self.swipeInfo[0] == -1:
-            self.swipeInfo[0] = event.x
-            self.swipeInfo[1] = event.y
+        if self.tmp_swipeInfo[0] == -1:
+            self.tmp_swipeInfo[0] = event.x
+            self.tmp_swipeInfo[1] = event.y
 
     def cbSwipeEnd(self, event):
-        if self.swipeInfo[0] != -1:
+        if self.tmp_swipeInfo[0] != -1:
             self.dbg("Swipe ended at ", event.x, event.y)
-            self.swipeInfo[2] = event.x
-            self.swipeInfo[3] = event.y
+            self.tmp_swipeInfo[2] = event.x
+            self.tmp_swipeInfo[3] = event.y
 
             #Send event
-            self.adbSendSwipe(list(self.swipeInfo))
+            self.adbSendSwipe(list(self.tmp_swipeInfo))
 
             #cleanup
-            self.swipeInfo = [-1, -1, -1, -1]
+            self.tmp_swipeInfo = [-1, -1, -1, -1]
 
     '''
     -----------------------Keyboard CALLBACKS
@@ -227,47 +202,127 @@ class LiveAndroidFeed():
         self.kbdSendText(txt)
 
     '''
-    -----------------------Telnet calls to Remote Keyboard
+    -----------------------Keyboard typing -> send to worker
     '''
     def kbdSendText(self, text):
         if len(text) == 0:
             return
-        self.wordToSend += str.encode(text)
-
-    #Use a periodic function to stop flooding the telnet when typing fast
-    def PER_kbdSendText(self):
-        self.parent.after(50, self.PER_kbdSendText)
-        if len(self.wordToSend) == 0:
-            return
-
-        self.keyboard.write(self.wordToSend)
-        self.wordToSend = b""
+        self.queue_kbd.put(text)
 
     '''
     -----------------------ADB CALLS TODO: replace with sendevent
     '''
     def adbSendTouch(self, x, y):
-        self.queue.put("adb shell input tap " + str(x) + " " + str(y))
+        self.queue_adb.put("input tap " + str(x) + " " + str(y))
 
     def adbSendSwipe(self, swipeCoords):
         if(swipeCoords[0] == -1):
             return
-        self.queue.put("adb shell input swipe " + str(swipeCoords[0]) + " " \
-                                                + str(swipeCoords[1]) + " " \
-                                                + str(swipeCoords[2]) + " " \
-                                                + str(swipeCoords[3]))
+        self.queue_adb.put("input swipe " + str(swipeCoords[0]) + " " \
+                                          + str(swipeCoords[1]) + " " \
+                                          + str(swipeCoords[2]) + " " \
+                                          + str(swipeCoords[3]))
 
     '''
     -----------------------DEBUG
     '''
     def dbg(self, *args):
-        print(*args)
+        #print(*args)
+        return
+
+
+class MinicapWorker(threading.Thread):
+    def __init__(self, queue, ip, port):
+        self.port = port
+        self.__queue = queue
+        self.frame = io.BytesIO()
+        self.keepRunning = True
+
+        self.adb_minicap = pexpect.spawn("adb shell LD_LIBRARY_PATH=/data/local/tmp/minicap-devel /data/local/tmp/minicap-devel/minicap -P 320x480@320x480/0")
+        time.sleep(5)
+        subprocess.call("adb forward tcp:" + str(port) + " localabstract:minicap", shell=True)
+
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock.connect((ip, port))
+
+        self.parseGlobalHeader()
+        threading.Thread.__init__(self)
+
+    def closeMinicap(self):
+        self.keepRunning = False
+
+    def cleanUp(self):
+        self.sock.shutdown(socket.SHUT_WR)
+        subprocess.call("adb forward --remove tcp:" + str(self.port), shell=True)
+        self.adb_minicap.close()
+        print("Minicap CLOSING!")
+
+    def parseGlobalHeader(self):
+        data = self.sockReceive(24)
+
+        vers = struct.unpack_from("B", data, 0)[0]
+        print("VERSION =", vers)
+
+        header_size = struct.unpack_from("B", data, 1)[0]
+        print("H SIZE =", header_size)
+
+        proc_pid = struct.unpack_from("I", data, 2)[0]
+        print("PID =", proc_pid)
+
+        x_real = struct.unpack_from("I", data, 6)[0]
+        y_real = struct.unpack_from("I", data, 10)[0]
+        print("Real display size: ", x_real, "x", y_real)
+
+        x_virt = struct.unpack_from("I", data, 14)[0]
+        y_virt = struct.unpack_from("I", data, 18)[0]
+        print("Virt display size: ", x_virt, "x", y_virt)
+
+        disp_or = struct.unpack_from("B", data, 22)[0]
+        print("ORIENTATION =", disp_or)
+        qirks = struct.unpack_from("B", data, 23)[0]
+        print("qirks =", qirks)
+
+    def getImageFromDevice(self):
+        data = self.sockReceive(4)
+        frame_size = struct.unpack_from("I", data, 0)[0]
+
+        self.frame.seek(0)
+        len_read = 0
+        while len_read < frame_size:
+            data = self.sockReceive(1024)
+            self.frame.write(data)
+            len_read += len(data)
+
+        self.frame.seek(0)
+        img = Image.open(self.frame)
+
+        return img
+
+    def sockReceive(self, length):
+        data, ancdata, msg_flags, address = self.sock.recvmsg(length, 1024)
+        for elem in ancdata:
+            print(elem)
+
+        return data
+
+    def run(self):
+        while self.keepRunning is True:
+            image = self.getImageFromDevice()
+
+            self.__queue.put(image)
+
+        self.cleanUp()
 
 
 class ADBWorker(threading.Thread):
     def __init__(self, queue):
         self.__queue = queue
+        self.adb_shell = pexpect.spawn("adb shell")
         threading.Thread.__init__(self)
+
+    def cleanUp(self):
+        self.adb_shell.close()
+        print("ADB CLOSING!")
 
     def run(self):
         while True:
@@ -275,9 +330,50 @@ class ADBWorker(threading.Thread):
             if item is None:
                 break
 
-            subprocess.call(item, shell=True)
+            self.adb_shell.sendline(item)
 
             self.__queue.task_done()
+
+        self.cleanUp()
+
+class KBDWorker(threading.Thread):
+    def __init__(self, queue, ip, port, sleep_time_ms=30):
+        self.__queue = queue
+        self.port = port
+
+        print("Connecting to keyboard....")
+        subprocess.call("adb forward tcp:" + str(port) + " tcp:" + str(port), shell=True)
+        self.keyboard = telnetlib.Telnet(ip, port, 5)
+        print("DONE!")
+        self.time_wait = sleep_time_ms #in ms
+
+        threading.Thread.__init__(self)
+
+    def cleanUp(self):
+        self.keyboard.close()
+        subprocess.call("adb forward --remove tcp:" + str(self.port), shell=True)
+        print("KDB CLOSING")
+
+    def run(self):
+        while True:
+            wordToSend = b""
+
+            try:
+                item = self.__queue.get(block=False)
+                if item is None:
+                    break
+
+                wordToSend += str.encode(item)
+                self.keyboard.write(wordToSend)
+
+                self.__queue.task_done()
+            except queue.Empty:
+                pass
+
+            time.sleep(self.time_wait/1000)
+
+        self.cleanUp()
+
 root = tk.Tk()
 laf = LiveAndroidFeed(root)
 root.mainloop()
